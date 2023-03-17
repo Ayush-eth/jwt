7ace685a1b381fe8075172edd3da8a214833b18d
Ayush <ayush.krsh9@gmail.com>
2023-03-18 01:58:19 +0530

    login implemented

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes= require('./routes/authRoutes')
+const { requireAuth } = require('./middleware/authMiddleware');
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://Batman:Ayush1907@cluster0.gq3kj0c.mongodb.net/test';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies',requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes)
+
+// cookies
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+  res.send('you got the cookies!');
+
+});
+
+app.get('/read-cookies', (req, res) => {
+
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+
+  res.json(cookies);
+
+});
