81904ab036cf9c6eb40e81679f8677b8b0f1519d
Ayush <ayush.krsh9@gmail.com>
2023-03-19 02:33:41 +0530

    JWT authentication complete

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
 
 const requireAuth = (req, res, next) => {
   const token   = req.cookies.jwt;
@@ -19,4 +21,26 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, 'your ex secret', async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+
+module.exports = { requireAuth, checkUser };
